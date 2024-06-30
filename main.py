982fe5b12f9d4d44dea59c5957c0a75b02f7ec17
Hypnos999 <flama2204@gmail.com>
2024-06-30 15:27:25 +0200

    ricerca dei terreni intersecati da una strada

--- main.py
+++ main.py
@@ -30,8 +30,19 @@ class TerrenoDatabase:
         terreni = self.terreni.find({"proprietario": codice_fiscale})
         return list(terreni)
 
-    def find_terreni_by_strada(self, strada_id):
-        terreni = self.terreni.find({"strade_intersezioni": strada_id})
+    def find_terreni_by_strada(self, strada_coordinate):
+        strada_geometria = {
+            "type": "LineString",
+            "coordinates": strada_coordinate
+        }
+        terreni = self.terreni.find({
+            "coordinate": {
+                "$geoIntersects": {
+                    "$geometry": strada_geometria
+                }
+            }
+        })
+
         return list(terreni)
 
     def aggiungi_terreno(self, terreno):
@@ -82,6 +93,7 @@ if __name__ == '__main__':
 
             input('\nPremi invio per continuare')
 
+
         elif scelta == 2:
             codice_fiscale = input("Inserisci il codice fiscale: ")
             terreni = db.find_terreni_by_proprietario(codice_fiscale)
@@ -99,6 +111,39 @@ if __name__ == '__main__':
 
             input('\nPremi invio per continuare')
 
+        ## trova terreni per strada
+        ## la strada puo' essere formata da piu' punti
+        ## l'idea di utilizzare gli id della strada penso abbia senso
+        ## solo se aggiungiamo un opzione a parte per salavare le strade
+        elif scelta == 3:
+            i = 1
+            cord = []
+            while True:
+                lat = input(f"Latitudine {i}° punto: ").lower().strip()
+                if lat == 'q': break
+
+                lon = input(f"Longitudine {i}° punto: ").lower().strip()
+                if lon == 'q': break
+
+                cord.append([float(lon), float(lat)])
+
+                i += 1
+
+            terreni = db.find_terreni_by_strada(cord)
+
+            if terreni:
+                print("\nTerreni trovati:")
+                for terreno in terreni:
+                    for k, v in terreno.items():
+                        if k in ["coordinate", "_id", 'type']:
+                            continue
+                        print(f'{k}: {v}')
+                    print('')
+
+            else:
+                print('\nNessun terreno trovato.')
+
+            input('\nPremi invio per continuare...')
 
         ## aggiungi terreno
         elif scelta == 4:
@@ -117,22 +162,20 @@ if __name__ == '__main__':
 
                 i += 1
 
+            cord = cord + [cord[0]]  # Chiude il poligono
             proprietario = input("Codice fiscale del proprietario: ")
             descrizione = input("Descrizione: ")
 
-            # Questi dati sono futili o li chiediamo all'utente?
-            # id_terreno = input("ID del terreno: ")
-            strade_intersezioni = input("ID delle strade intersecate (separate da virgola): ").split(",")
-
             terreno = {
-                "type": "Polygon",
-                "coordinates": [cord],
+                "coordinate": {
+                    "type": "Polygon",
+                    "coordinates": [cord],
+                },
                 "proprietario": proprietario,
                 "descrizione": descrizione,
-                # "id_terreno": id_terreno,
-                "strade_intersezioni": strade_intersezioni
             }
 
             if db.check_terreno_occupato(cord):
-                print("Il terreno è già occupato. Impossibile occuparlo")
+                print("\nIl terreno è già occupato. Impossibile caricare i dati sul database")
             else: db.aggiungi_terreno(terreno)
+            input('\nPremi invio per continuare...')
